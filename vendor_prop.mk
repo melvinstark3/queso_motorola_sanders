2cd0d8d0f6bcd034e4f0fcf899523f7091e1e9f1
Suren Baghdasaryan <surenb@google.com>
2018-01-09 15:47:40 -0800

    sanders: Set lmkd configuration properties
    Bug: 63631020
    
    Change-Id: I16a183e8e4625e8b98cefdc9c8a7b650556a2b55
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    Signed-off-by: Melvin <kartik3163@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -202,6 +202,17 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.location.osnlp.package=com.google.android.gms \
     ro.location.osnlp.region.package=""
 
+# Set lmkd options
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.lmk.low=1001 \
+    ro.lmk.medium=0 \
+    ro.lmk.critical=0 \
+    ro.lmk.critical_upgrade=false \
+    ro.lmk.upgrade_pressure=100 \
+    ro.lmk.downgrade_pressure=100 \
+    ro.lmk.kill_heaviest_task=true \
+    ro.lmk.kill_timeout_ms=100
+
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
     media.aac_51_output_enabled=true \
