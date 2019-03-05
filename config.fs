0591b8205b7949ac6aadd8d283d03a7ac3b26d3e
Shinru Han <shinruhan@google.com>
2019-03-05 20:08:14 +0800

    sanders: gps: Start loc_launcher as gps user
    Start loc_launcher as gps user and set uid/gid of loc_launcher
    to gps and enable SETUID and SETGID caps for loc_launcher, so
    that we can start loc_launcher as gps user instead of root and
    still have capabilities for SETUID and SETGID. Also remove not
    required permissions groups for loc_launcher.
    
    Test: build & loc_laucher is seen
    Bug: 127433614
    CRs-Fixed: 2192881
    
    Change-Id: I95a30854949bcca556cd6579ecb3018092e6a822
    Signed-off-by: Melvin <kartik3163@gmail.com>

--- config.fs
+++ config.fs
@@ -85,6 +85,18 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
+[vendor/bin/loc_launcher]
+mode: 0755
+user:  AID_GPS
+group: AID_GPS
+caps: SETUID SETGID
+
+[system/vendor/bin/loc_launcher]
+mode: 0755
+user:  AID_GPS
+group: AID_GPS
+caps: SETUID SETGID
+
 [vendor/bin/mm-qcamera-daemon]
 mode: 0700
 user: AID_CAMERA
