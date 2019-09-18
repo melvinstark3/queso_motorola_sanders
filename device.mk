66906f634bfe1388c247b186315789fd1a9c9b48
Garret Kelly <gdk@google.com>
2019-09-18 18:36:27 +0600

    sanders: Set BOOT_SECURITY_PATCH during build
    This property is now used during boot to provide the TEE-backed
    keymaster with the correct boot SPL.  Set it to the platform SPL until
    it diverges as part of an update.
    
    Bug: 119549677
    Test: verified boot.img has property using avbtool
    Change-Id: I6d96404918cf18d0fee79c701394b39baf349c53
    Signed-off-by: Henrique Pereira <hlcpereira@outlook.com.br>
    Signed-off-by: Melvin <kartik3163@gmail.com>

--- device.mk
+++ device.mk
@@ -14,6 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Set boot SPL
+BOOT_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Vendor properties
