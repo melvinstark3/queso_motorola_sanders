1234ef58b762dede46bc8245a94fb765e37b94cf
Melvin <kartik3163@gmail.com>
2020-03-10 13:43:53 +0530

    sanders: Include Additional Trim Properties
    Signed-off-by: Melvin <kartik3163@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -385,6 +385,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 #Trim properties
 PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.qti.sys.fw.use_trim_settings=true \
+    ro.vendor.qti.sys.fw.empty_app_percent=50 \
+    ro.vendor.qti.sys.fw.trim_empty_percent=100 \
+    ro.vendor.qti.sys.fw.trim_cache_percent=100 \
     ro.vendor.qti.sys.fw.trim_enable_memory=2147483648
 
 # Touch response
